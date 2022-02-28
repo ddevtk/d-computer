0d608c3558da90ce69d1bf30e5e4b17eb72d982e
duongbui123 <buiducduong2342000@gmail.com>
2022-02-28 19:46:58 +0700

    category CRUD client

---
+++ client/src/api/categoryApi.js
@@ -0,0 +1,40 @@
+import axios from 'axios';
+
+export const getAllCategories = async () =>
+  await axios.get(`${process.env.REACT_APP_API}/categories`);
+
+export const getCategory = async (slug) =>
+  await axios.get(`${process.env.REACT_APP_API}/categories/${slug}`);
+
+export const createCategory = async (name, token) =>
+  await axios.post(
+    `${process.env.REACT_APP_API}/categories`,
+    { name },
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+
+export const updateCategory = async (slug, name, token) =>
+  await axios.put(
+    `${process.env.REACT_APP_API}/categories/${slug}`,
+    { name },
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+
+export const deleteCategory = async (slug, token) =>
+  await axios.delete(
+    `${process.env.REACT_APP_API}/categories/${slug}`,
+    {},
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
